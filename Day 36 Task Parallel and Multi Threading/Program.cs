57d8f7633444aa8a1f9e7056dc761ce6fcb4ab9e
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-04-10 02:12:12 +0530

    added Day 36 Task Parallel and Multi Threading Problems

---
+++ Day 36 Task Parallel and Multi Threading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_36_Task_Parallel_and_Multi_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
