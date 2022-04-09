9dae871a6f231fffa5161441f32e82a6c78c7560
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-04-10 03:31:48 +0530

    added UC 5 Added Payroll Detials

---
+++ Day 36 Task Parallel and Multi Threading/PayrollDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_36_Task_Parallel_and_Multi_Threading
+{
+    public class PayrollDetails
+    {
+        public int EmployeeID { get; set; }
+        public double BasicPay { get; set; }
+        public double Deductions { get; set; }
+        public double TaxablePay { get; set; }
+        public double Tax { get; set; }
+        public double NetPay { get; set; }
+        //UC 4
+        public PayrollDetails(int EmployeeID, double BasicPay, double Deductions, double TaxablePay, double Tax, double NetPay)
+        {
+            this.EmployeeID = EmployeeID;
+            this.BasicPay = BasicPay;
+            this.Deductions = Deductions;
+            this.TaxablePay = TaxablePay;
+            this.Tax = Tax;
+            this.NetPay = NetPay;
+
+        }
+    }
+    
+}
---
+++ Day 36 Task Parallel and Multi Threading/PayrollOperations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_36_Task_Parallel_and_Multi_Threading
+{
+    public class PayrollOperations
+    {
+        public List<PayrollDetails> PayrollDetailList = new List<PayrollDetails>();
+
+        //UC 5 without Thread
+        public void addPayrollWithoutThread(List<PayrollDetails> payrollDataList)
+        {
+            payrollDataList.ForEach(payrollData =>
+            {
+                Stopwatch Time = new Stopwatch();
+                Time.Start();
+                Console.WriteLine("Payment Being Added  :" + payrollData.BasicPay);
+                this.addToPayroll(payrollData);
+                Time.Stop();
+                Console.WriteLine("Payment added : " + payrollData.BasicPay + " ( Duration  : " + Time.Elapsed + ")");
+            });
+            Console.WriteLine(this.PayrollDetailList.ToString());
+        }
+        //UC 5, With Thread
+        public void addPayrolllWithThread(List<PayrollDetails> payrollDataList)
+        {
+            payrollDataList.ForEach(payrollData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Stopwatch Time = new Stopwatch();
+                    Time.Start();
+
+                    Console.WriteLine("Payment Being Added  :" + payrollData.BasicPay);
+                    this.addToPayroll(payrollData);
+                    Time.Stop();
+                    Console.WriteLine("Payment added : " + payrollData.BasicPay + " ( Duration : " + Time.Elapsed + ")");
+                });
+                thread.Start();
+            });
+            Console.WriteLine(this.PayrollDetailList.Count);
+        }
+        //UC 6 update Basic Pay Without Thread
+        public void UpdatePayRollWithThread(List<PayrollDetails> payrollDataList)
+        {
+
+        }
+        public void addToPayroll(PayrollDetails pay)
+        {
+            PayrollDetailList.Add(pay);
+
+        }
+
+    }
+}
